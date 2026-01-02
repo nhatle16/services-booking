86d6f9dbc1d30ac08844ee4727d91ea8b16e49be
Le Nhat <nhatle.stu@gmail.com>
2026-01-02 17:55:46 -0600

    Implemented modular routing for service-specific routes

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 const express = require('express');
 const app = express();
 
+const hotelRouter = require('./controllers/hotel.controller');
+const flightRouter = require('./controllers/flight.controller');
+const bookingRouter = require('./controllers/booking.controller');
+
 app.use(express.json());
 
 // Sample route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.use('/api/v1', hotelRouter);
+app.use('/api/v1', flightRouter);
+app.use('/api/v1', bookingRouter);
 
 module.exports = app;
